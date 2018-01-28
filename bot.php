0f13e9bc11631851c83c012698388eeff686ff1a
Kevin1507 <35886220+Kevin1507@users.noreply.github.com>
2018-01-28 14:04:23 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -45,7 +45,7 @@ function shalat($keyword) {
     $response = Unirest\Request::get("$uri");
 
     $json = json_decode($response->raw_body, true);
-    $result = "Jadwal Shalat Sekitar ";
+    $result = "Jadwal Shalat By Kevin Juliano ";
 	$result .= $json['location']['address'];
 	$result .= "\nTanggal : ";
 	$result .= $json['time']['date'];
